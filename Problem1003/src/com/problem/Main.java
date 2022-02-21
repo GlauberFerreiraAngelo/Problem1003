63c2fc96270a3120cf36bc374ec0fb5877845444
GlauberFerreiraAngelo <42098584+GlauberFerreiraAngelo@users.noreply.github.com>
2022-02-21 11:31:25 -0300

    Add files via upload

---
+++ Problem1003/src/com/problem/Main.java
@@ -0,0 +1,17 @@
+package com.problem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int A, B, SOMA;
+		
+		A = 30;
+		B = 10;
+		
+		SOMA = A + B;
+		
+		System.out.println("SOMA = "+SOMA);
+		
+	}
+}
